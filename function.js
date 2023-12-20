928df99ef009f95d0213c68689012d270a8cfe69
PonomarenkoNina <ninulka@ukr.net>
2023-12-20 14:04:11 +0200

    change of comment language

--- function.js
+++ function.js
@@ -9,11 +9,11 @@ bark("rover", 23);
 bark("spot", 13);
 bark("spike", 53);
 bark("lady", 17);
-// Упражнение
-bark("juno", 20); // juno says woof woof, так как 20 не больше 20, false
-bark("scottie", -1); // scottie says woof woof, так как -1 не больше 20
-bark("dino", 0, 0); // dino says woof woof, тут 3 аргумента
-bark("fido", "20"); // fido says woof woof, тут js сравнит строку с числом
-bark("lady", 10); // lady says woof woof, 10 меньше 20
-bark("bruno", 21); // bruno says WOOF WOOF, 21 больше 20, true
-// все работает!Ошибок нет!
+// Exercise 1
+bark("juno", 20); // juno says woof woof, since 20 is not more than 20, false
+bark("scottie", -1); // scottie says woof woof, since -1 is not more than 20
+bark("dino", 0, 0); // dino says woof woof, there are 3 arguments here
+bark("fido", "20"); // fido says woof woof, here js will compare the string with the number
+bark("lady", 10); // lady says woof woof, 10 less than 20
+bark("bruno", 21); // bruno says WOOF WOOF, 21 is over 20,, true
+// Everything works! No errors!
