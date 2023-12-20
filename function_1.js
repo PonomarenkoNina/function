7dc126f4066f4d17223d04b1472ad5004e325c75
PonomarenkoNina <ninulka@ukr.net>
2023-12-20 13:56:49 +0200

    change of comment language

--- function_1.js
+++ function_1.js
@@ -1,4 +1,4 @@
-// Упражнение 1
+// Exercise 1
 function whatShellWear(temp) {
   if (temp < 60) {
     console.log("Wear a jacket");
@@ -13,13 +13,13 @@ whatShellWear(50);
 whatShellWear(80);
 whatShellWear(60);
 
-// Лишние  ,  ,  ,  }  } temperature
-// На консоле JavaScript должно выводиться:
+// Superfluous  ,  ,  ,  }  } temperature
+// On the console JavaScript:
 // Wear a jacket
 // Wear a t-shirt
 // Wear a sweater
 
-// Упражнение 2
+// Exercise 2
 function dolt(param) {
   param = 2;
 }
@@ -27,30 +27,33 @@ var test = 1;
 dolt(test);
 console.log(test);
 
-// На консоль выведет 1, так как мы
-// передаем переменную test в функцию dolt
-// то есть dolt присваивается новое значение param
+// It will output 1 to the console, since we
+// pass the test variable to the dolt function
+// that is, dolt is assigned a new value to param
 
-// Упражнение 3
-// Есть фрагмент кода JavaScript с переменными, определениями функций и вызовами функций.
-// Найти все переменные, функции, аргументы и параметры.
+// Exercise 3
+// There is a piece of JavaScript code with variables, function definitions and function calls.
+// Find all variables, functions, arguments and parameters.
 function pet(dogName, age) {
   var years = age * 7;
   console.log(dogName + " is " + years + " years old ");
 }
 var myDog = "Fido";
 dogYears(myDog, 4);
+// Fido is 28 years old
 
 function makeTea(cups, tea) {
   console.log("Brewing" + cups + " cups of " + tea);
 }
 var guests = 3;
 makeTea(guests, "Earl Grey");
+// Brewing 3 cups of Earl Grey
 
 function secret() {
   console.log("The secret of life is 42");
 }
 secret();
+// The secret of life is 42
 
 function speak(kind) {
   var defaultSound = "";
@@ -64,14 +67,10 @@ function speak(kind) {
 }
 var pet = promt("Enter a type of pet: ");
 speak(pet);
-
-// Переменые: years myDog guests defaultSound pet
-// Функции: pet makeTea secret speak
-// Аргументы: myDog, 4  guests, "Earl Grey"
-// Параметры: dogName, age  cups, tea  kind
-// Встроенные функции: console.log  alert prompt
-
-// Fido is 28 years old
-// Brewing 3 cups of Earl Grey
-// The secret of life is 42
 // Enter a type of pet: dog or cat
+
+// Variables: years myDog guests defaultSound pet
+// Functions: pet makeTea secret speak
+// Arguments: myDog, 4  guests, "Earl Grey"
+// Options: dogName, age  cups, tea  kind
+// Built-in functions:  console.log  alert  prompt
